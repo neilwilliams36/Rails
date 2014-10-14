401954037ebd15ae3157788634a3ca81e9231b1e
neilwilliams36 <nwilliams36@yahoo.com>
2014-10-14 13:47:13 +0930

    Finish Chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe "Static Pages" do
   describe "Home" do
-    it "should have the content Sample" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample')
-      expect(page).to have_content('This is the')
-      expect(page).to have_title("Sample")
+    before {visit root_path}
+   it "should have the correct home page" do
+     expect(page).to have_content('Sample')
+     expect(page).to have_title('Sample')
+     expect(page).to_not have_title('| Home')
+   end
     end
   end
 
   describe "Help" do
     it "should have the content Help" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
       expect(page).to have_title("Sample")
     end
@@ -23,9 +23,18 @@ describe "Static Pages" do
   describe "About" do
     it "should have the content Help" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About')
       expect(page).to have_title("Sample")
     end
   end
+
+  describe "Contact" do
+    it "should have the content Contact" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit contact_path
+      expect(page).to have_content('Contact')
+      expect(page).to have_title("Sample")
+    end
 end
+
